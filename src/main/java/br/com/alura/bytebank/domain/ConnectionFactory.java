b8c7f2f76486e99a45da113d07483148d2b2ebbe
F8527576 Renan Jose De Souza Linhares <renanlinhares@bb.com.br>
2024-11-22 13:53:33 -0300

    first commit

---
+++ src/main/java/br/com/alura/bytebank/domain/ConnectionFactory.java
@@ -0,0 +1,29 @@
+package br.com.alura.bytebank.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection recuperarConexao() {
+        try {
+            return DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=wsl");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+// public static void main(String[] args) {
+// try {
+// Connection connection = DriverManager
+// .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=wsl");
+// System.out.println("Recuperei a conexão");
+
+// connection.close();
+// } catch (SQLException e) {
+// System.out.println(e);
+// }
+// }
